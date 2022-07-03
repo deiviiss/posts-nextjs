a4c67daaabdb0f6e567bf6d41e8f8670a658dc7f
David Hilera <david.hilera@hotmail.com>
2022-07-03 08:03:40 -0500

    [fix] eslinrc deploy vercel

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,12 @@ module.exports = {
     node: true,
     es6: true,
   },
-  extends: ['eslint:recommended', 'plugin:jsx-a11y/recommended', 'plugin:prettier/recommended', 'next', "next/babel", 'next/core-web-vitals'],
+  extends: [
+    'eslint:recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:prettier/recommended',
+    'next', "next/babel",
+    'next/core-web-vitals'],
   rules: {
     semi: ['error', 'always'],
     'prettier/prettier': 0,
